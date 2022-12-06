4666972cc143d00114e0bb67043fae5f94cef40b
jakobssonje <jens.jakobson@cgi.com>
2022-12-06 10:39:23 +0100

    dao01 ok

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,41 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun getAllElves(input: List<String>): List<List<Int>> {
+        val allElves = mutableListOf<List<Int>>()
+        var perElf = ArrayList<Int>()
+        var firstRowPerElf = true
+        for (s in input) {
+            if (s == "") {
+                perElf = ArrayList()
+                firstRowPerElf = true
+            } else {
+                if (firstRowPerElf) {
+                    allElves.add(perElf)
+                    firstRowPerElf = false
+                }
+                perElf.add(s.toInt())
+            }
+        }
+        return allElves
     }
 
+    fun part1(input: List<String>): Int {
+        return getAllElves(input)
+            .maxOf { it.sum() }
+
+    }
+
+
     fun part2(input: List<String>): Int {
-        return input.size
+        return getAllElves(input)
+            .sortedByDescending { it.sum() }
+            .take(3)
+            .sumOf { it.sum() }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
