3f772830940f4f337e1e1cb8417b98ff551feb0b
jakobssonje <jens.jakobson@cgi.com>
2022-12-13 11:12:02 +0100

    day 07 part1 ok refactor

--- src/Day07.kt
+++ src/Day07.kt
@@ -2,17 +2,24 @@ class MyDir(
     val name: String,
     val subDirs: MutableList<MyDir> = mutableListOf(),
     val parent: MyDir?,
-    val files: MutableList<MyFile> = mutableListOf(),
+    private val files: MutableList<MyFile> = mutableListOf(),
     var dirSize: Int = 0,
 ) {
-    fun addFile(myFile: MyFile) {
-        files.add(myFile)
-        dirSize += myFile.size
+    fun addFile(size: Int, name: String): MyDir {
+        files.add(MyFile(size, name))
+        dirSize += size
         var parentDir = parent
         while (parentDir != null) {
-            parentDir.dirSize += myFile.size
+            parentDir.dirSize += size
             parentDir = parentDir.parent
         }
+        return this
+
+    }
+
+    fun addDirectory(name: String): MyDir {
+        subDirs.add(MyDir(name = name, parent = this))
+        return this
 
     }
 }
@@ -22,31 +29,36 @@ data class MyFile(val size: Int, val name: String)
 fun main() {
 
 
-    fun parseFileSystem(input: List<String>): MyDir {
-        val fileSystem = MyDir(name = "/", parent = null)
-
-        var currentDir = fileSystem
-        for (row in input) {
-            val data = row.split(" ")
-            if (data[0] == "$") {
-                val command = data[1]
-                if (command == "cd") {
-                    val dir = data[2]
-                    currentDir = if (dir == "..") {
-                        currentDir.parent ?: throw IllegalStateException("No parent")
-                    } else {
-                        currentDir.subDirs
-                            .find { it.name == dir }
-                            ?: if (currentDir.name == dir) currentDir else throw IllegalArgumentException("Could not find $dir")
-                    }
-                }
-            } else if (data[0] == "dir") {
-                currentDir.subDirs.add(MyDir(name = data[1], parent = currentDir))
+    fun doCommand(data: List<String>, cwd: MyDir): MyDir {
+        val command = data[1]
+        return if (command == "cd") {
+            val dir = data[2]
+            if (dir == "..") {
+                cwd.parent ?: throw IllegalStateException("No parent")
             } else {
-                currentDir.addFile(MyFile(data[0].toInt(), data[1]))
+                cwd.subDirs
+                    .find { it.name == dir }
+                    ?: if (cwd.name == dir) cwd
+                    else throw IllegalArgumentException("Could not find $dir in ${cwd.name}")
             }
+        } else cwd
+
+    }
+
+    fun parseFileSystem(input: List<String>): MyDir {
+        val root = MyDir(name = "/", parent = null)
+        var cwd = root
+
+        input.forEach { row ->
+            val data = row.split(" ")
+            cwd = when (data[0]) {
+                "$" -> doCommand(data, cwd)
+                "dir" -> cwd.addDirectory(data[1])
+                else -> cwd.addFile(data[0].toInt(), data[1])
+            }
+
         }
-        return fileSystem
+        return root
 
     }
 
